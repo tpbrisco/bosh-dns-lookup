a5425a389d8de299f3dc423de7cd2a9343940a24
tpbrisco <4596011+tpbrisco@users.noreply.github.com>
2024-06-08 07:52:52 -0400

    use strong typing, for better swagger output

--- boshdns.py
+++ boshdns.py
@@ -5,9 +5,9 @@ import dns.resolver
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 import starlette.status as status
+from pydantic import BaseModel
 
 resolver = dns.resolver.Resolver()
-resolver.timeout = 5
 
 # determine dns lookup behavior
 am_cf_deployed = False
@@ -21,45 +21,70 @@ app = FastAPI(
     version='0.1.0')
 
 
-def get_dns_rr(name: str) -> list:
+# get_dns_rr return - include data and/or error message
+class DnsAns(BaseModel):
+    reason: str | None
+    addresses: list
+
+
+# main entry point data returned
+class BoshDnsAns(BaseModel):
+    query: str | None = None
+    instance_group: str
+    query_flags: str
+    deployment: str
+    network: str
+    addresses: list
+    reason: str | None = None
+
+
+# simple dns lookup - return data and/or error code
+def get_dns_rr(name: str) -> DnsAns:
     '''get_dns_rr(hostname) - get 'A' records and return an array of them'''
+    rr = DnsAns(reason=None, addresses=list())
     try:
         ans = resolver.resolve(name, 'A')
     except dns.resolver.NXDOMAIN as e:
         # nothing to do, just no data is available
         # would be better to return "not found", but for simplicity ...
-        return list()
+        rr.reason = str(e)
+        return rr
     except dns.resolver.NoNameservers as e:
-        return list()
-    resp = list()
+        rr.reason = str(e)
+        return rr
     for r in ans:
-        resp.append(r.to_text())
-    return resp
+        rr.addresses.append(r.to_text())
+    return rr
 
 
+# just redirect to swagger UI if no query
 @app.get("/", include_in_schema=False)
 def main():
     '''redirect to swagger interface, could be /redoc for ReDoc format'''
     return RedirectResponse(url="/docs", status_code=status.HTTP_302_FOUND)
 
 
+# DNS lookup - use BOSH DNS format if we're deployed in cloud foundry,
+# otherwise return a standard DNS lookup (for testing, dev, etc)
 @app.get("/lookup/{instance_group}")
 def dns_lookup(instance_group: str,
                query_flags: str = "s4",
                deployment: str = '*',
-               network: str = "*") -> dict:
+               network: str = "*") -> BoshDnsAns:
+    bd_ans = BoshDnsAns(
+        instance_group=instance_group,
+        query_flags=query_flags,
+        deployment=deployment,
+        network=network,
+        addresses=list())
     # non-CF behavior is to just look up a hostname
-    name = instance_group
+    bd_ans.query = instance_group
     if am_cf_deployed:
         # if we're in CF, formulate a BOSH DNS query
-        name = f"q-{query_flags}.{instance_group}.{network}.{deployment}.bosh"
+        bd_ans.query = f"q-{query_flags}.{instance_group}.{network}.{deployment}.bosh"
 
-    addresses = get_dns_rr(name)
-    return {
-        "name": name,
-        "instance_group": instance_group,
-        "query_flags": query_flags,
-        "deployment": deployment,
-        "network": network,
-        "addresses": addresses
-    }
+    # addresses = get_dns_rr(name)
+    ans = get_dns_rr(bd_ans.query)
+    bd_ans.addresses = ans.addresses.copy()
+    bd_ans.reason = ans.reason
+    return bd_ans
